116d5cebcd3cce504fdb05478390a7a3d7a392ba
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-04 15:17:25 -0600

    add clock on page

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    //display current day
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    //save button event listener
+    $(".saveBtn").on("click", function() { 
+        // create variable for textarea
+        var text = $(this).siblings(".description").val();
+        //create variable for time-slot
+        var time = $(this).parent().attr("id"); 
+
+        //save variables in local storage
+        localStorage.setItem(time, text);
+    });
+
+    //display saved local storage data
+    $("#block9 .description").val(localStorage.getItem("block9"));
+    $("#block10 .description").val(localStorage.getItem("block10"));
+    $("#block11 .description").val(localStorage.getItem("block11"));
+    $("#block12 .description").val(localStorage.getItem("block12"));
+    $("#block13 .description").val(localStorage.getItem("block13"));
+    $("#block14 .description").val(localStorage.getItem("block14"));
+    $("#block15 .description").val(localStorage.getItem("block15"));
+    $("#block16 .description").val(localStorage.getItem("block16"));
+    $("#block17 .description").val(localStorage.getItem("block17"));
+});
