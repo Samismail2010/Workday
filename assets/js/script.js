55c304357f862750ecda64fcd84e6babaeb612ac
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-04 16:20:21 -0600

    local storage bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
         var time = $(this).parent().attr("id"); 
 
         //save variables in local storage
-        localStorage.setItem(time, text)
+        localStorage.setItem(time, text);
+    
     });
 
     //display saved local storage data
